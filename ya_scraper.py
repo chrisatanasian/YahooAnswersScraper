232d797830315d4e53cb953b576ea2fa07c10ae6
Chris Atanasian <catanasian@gmail.com>
2015-10-04 01:34:43 -0700

    Add command line flags with argparse

--- ya_scraper.py
+++ ya_scraper.py
@@ -2,29 +2,38 @@
 
 import sys, time, os.path
 import selenium_methods, soup_methods, db_methods
+import argparse
+
+DB_NAME     = 'ya.db'
+FILE_NAME   = 'questions.html'
+HTML_CLASS  = 'Fz-14 Fw-b Clr-b Wow-bw title'
+ONE_DAY_AGO = time.time() - 60 * 60 * 24;
 
 def main():
-    db_name     = 'ya.db'
-    fname       = 'questions.html'
-    html        = ''
-    html_class  = 'Fz-14 Fw-b Clr-b Wow-bw title'
-    one_day_ago = time.time() - 60 * 60 * 24;
-    url         = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', required=True,
+                        help='questions category URL')
+    parser.add_argument('-d', '--get_descriptions', action='store_true',
+                        help='get descriptions of questions')
+    args = parser.parse_args()
 
-    if not os.path.isfile(fname) or os.path.getctime(fname) < one_day_ago:
+    url = vars(args)['url']
+    get_descriptions = vars(args)['get_descriptions']
+
+    if not os.path.isfile(FILE_NAME) or os.path.getctime(FILE_NAME) < ONE_DAY_AGO:
         html = selenium_methods.get_questions_html(url, 50, 0.75)
-        f = open('questions.html', 'w')
+        f = open(FILE_NAME, 'w')
         f.write(html.encode('utf-8'))
         f.close()
     else:
-        f = open('questions.html', 'r')
+        f = open(FILE_NAME, 'r')
         html = str(f.readlines())
         f.close()
 
     soup = soup_methods.convert_html_to_soup(html)
-    questions = soup_methods.get_questions(soup, html_class, False)
+    questions = soup_methods.get_questions(soup, HTML_CLASS, get_descriptions)
 
-    db_methods.insert_into_db(db_name, questions)
+    db_methods.insert_into_db(DB_NAME, questions)
 
 if __name__ == "__main__":
     main()
